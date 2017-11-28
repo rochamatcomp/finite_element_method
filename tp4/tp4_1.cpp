a95bf8da1674af803bad79cf1f57b75f6375d006
André Rocha <rocha.matcomp@gmail.com>
2017-11-28 10:51:02 +0100

    Fixing mesh file generation. Adding the file mesh.msh as example (4 number os grid points). Including the arguments of make run.

--- tp4/tp4_1.cpp
+++ tp4/tp4_1.cpp
@@ -11,27 +11,36 @@
 
 using namespace std;
 
+/**
+ * Filling the mesh file with the uniform grid.
+ * @param file Stream of the file to writing.
+ * @param total Number of grid points. 
+ * @return  
+ */
 void fill(ofstream& file, double total)
 {
-  int vertices = total + 1;
-  int edges = total;
-  int label = 0; // label of the element of the grid.
+  int vertices = total + 1; // quantity of the vertices.
+  int edges = total; // quantity of the edges.
+  int label = 0; // label of the grid element.
   int index = 0;
-  int left = 0; // number of vertices of a edge, in the left side.
-  int right = 0; // number of vertices of a edge, in the right side.
-  int border0 = 0; // number of vertices of a edge (0, 1), in the 0 side.
-  int border1 = 0; // number of vertices of a edge (0, 1), in the 1 side.
+  int left = 0; // number of edge vertice, in the left side.
+  int right = 0; // number of edge vertice, in the right side.
+  int start = 0; // number of start vertice.
+  int end = total ; // number end vertice.
   double element = 0.0; // element of the grid (xi).
-  
+ 
   // Writing header
-  file << vertices << "\t" << edges << "\t" << 2;
+  file << vertices << "\t" << edges << "\t" << 2 << endl;
 
   // Writing the elements
   for(index = 0; index < vertices; index++)
   {
     element = index / total;
+
+    // Label 1 to limits elements and label 0 to inside elements.
     label = (element == 0 || element == 1) ? 1 : 0;
-    file << element << "\t" << label;
+    
+    file << element << "\t" << label << endl;
   }
 
   // Writing the numbers of vertices of each edge.
@@ -39,43 +48,42 @@ void fill(ofstream& file, double total)
   {
     left = index;
     right = index + 1;
-    file << left << "\t" << right << "\t" << 0;
+    file << left << "\t" << right << "\t" << 0 << endl;
   }
 
-  file << border0 << "\t" << 0;
-  file << border1 << "\t" << 0;
+  // Writing the number of the limits vertices.
+  file << start << "\t" << 0 << endl;
+  file << end << "\t" << 0;
 }
 
 int main(int argc, char* argv[])
 {
-  int size = 0; // mesh size
-  double total = 0.0;
+  double total = 0.0; // Number of grid points
   string filename = "mesh.msh";
   ofstream file;
   
   if(argc < 2)
   {
     // Help
-    cout <<"Usage: "<< argv[0] <<" <mesh size> [<mesh filename>]" << endl;
+    cout <<"Usage: "<< argv[0] <<" <number of grid points> [<mesh filename>]" << endl;
+    exit(0);
   }
-
-  size = atoi(argv[1]);
   
-  if(argc == 3)
-  {
-    filename = argv[2] + ".msh";
-  }
+  total = atof(argv[1]);
     
-  if(size <= 0)
+  if(total <= 0.0)
   {
     cout << "Impossible create the mesh with non positive size." << endl;
     exit(1);
   }
 
-  // Total of the elements on the regular grid.
-  total = 1 / (double)size;
+  if(argc == 3)
+  {
+    filename = string(argv[2]) + ".msh";
+  }
   
-  if(file.open (filename))
+  file.open(filename);
+  if(file.is_open())
   {
     fill(file, total);
     file.close();
@@ -85,6 +93,6 @@ int main(int argc, char* argv[])
     cout << "Impossible open the file: " << filename << "" << endl;
     exit(1);
   }
-
+  
   return 0;
 }
